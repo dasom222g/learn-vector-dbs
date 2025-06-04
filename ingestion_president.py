8657ef7492373d6384a9b69c603a733e0a9409c0
dasom222g <dasom228@gmail.com>
2025-06-04 11:50:22 +0900

    feat: add retrieval rag chain

---
+++ ingestion_president.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+if __name__ == '__main__':
+    print('hello')
+    # =============================== data injection ===============================
+
+    # 1. 랭체인 문서 불러오기
+    file_path = '/Users/dasom/somi/github.com/dasom222g/learn-vector-dbs/president_doc.txt'
+    loader = TextLoader(file_path=file_path) # 문서 로더 객체
+    document = loader.load() # 랭체인 문서
+
+    # 2. 청킹하기
+    chunk_size = 1000
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, separators=["\n\n", "\n", " ", ""], chunk_overlap=0) # 텍스트 분할 객체
+    chunked_docs = text_splitter.split_documents(document) # 청킹한 데이터
+    # print(chunked_docs)
+
+    # for i, data in enumerate(chunked_docs):
+    #     print(f'{i + 1}번째 청크 사이즈: {len(data.page_content)}')
+
+    # 3. 임베딩 모델의 정보를 가진 임베딩 객체 가져오기
+    embeddings = OpenAIEmbeddings(model='text-embedding-ada-002', api_key=os.getenv('OPENAI_API_KEY')) # api_key 옵션 명시 안해줘도 자동인식
+
+    # 4-1. 벡터스토어 생성 및 저장
+    vector_store = PineconeVectorStore.from_documents(documents=chunked_docs, embedding=embeddings, index_name=os.getenv('INDEX_NAME_PRESIDENT'))
+
+    # 4-2. 기존 스토어에 추가
+    # vector_store.add_documents(documents=chunked_docs)
--- main.py
+++ main.py
@@ -2,35 +2,63 @@ import os
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
+from openai import responses
 
 # 환경변수 로드
 load_dotenv()
+
+# query = 'What is pinecone in machine learning? 한국어로 답변하세요'
+query = '이재명의 슬로건과 정치적 배경 등 아는대로 알려주세요'
 
 
 def basic_rag():
     # 1. 벡터스토어 초기화(index name, embeddigs)
     embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
-    vetorstor = PineconeVectorStore(embedding=embeddings, index_name=os.getenv('INDEX_NAME'))
+    # vectorstore = PineconeVectorStore(embedding=embeddings, index_name=os.getenv('INDEX_NAME'))
+    vectorstore = PineconeVectorStore(embedding=embeddings, index_name=os.getenv('INDEX_NAME_PRESIDENT'))
 
     # 2. langchain-ai/retrieval-qa-chat 프롬프트 로드
     prompt = hub.pull('langchain-ai/retrieval-qa-chat')  # input_variables=['context', 'input']
     print(f'prompt: \n {prompt}')
 
     # 3. 체인 설정
-    # create_stuff_documents_chain: 검색된 문서들을 하나로 합쳐서 LLM에게 전달하는 체인을 만드는 함수
     # create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
     # 3-1. 체인 생성 (초기화)
     llm = ChatOpenAI(model='gpt-4o-mini')
 
-    # 🔥🔥🔥🔥여기부터
-    # combined_docs = create_stuff_documents_chain(llm=llm)
-
+    # 유사도 검색
+    search_docs = vectorstore.similarity_search(query=query, k=3)
+    # 검색된 문서들을 하나로 합칠 객체 정의
+    combine_docs_chain = create_stuff_documents_chain(llm, prompt)
     # 3-2. 체인 실행 (이때 문서 합쳐짐)
+    # ⭐️방법 1
+    response_stuff = combine_docs_chain.invoke({
+        "context": search_docs, # 이때 여러개의 문서 함쳐짐
+        "input": query
+    })
 
+    print(f'🔥🔥최종답변 combine_docs_chain: {response_stuff}') # ⭐⭐문서에 fit한 답변 - str
+
+    # ⭐️방법 2
     # 4. 완전한 rag체인 생성 및 질문
 
+    # 검색기 준비
+    retrieval = vectorstore.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever=retrieval, combine_docs_chain=combine_docs_chain)
+    response_retrieval = retrieval_chain.invoke({
+        'input': query # 질문만 넘기면 끝
+    })
+
+    print(f'🔥🔥최종답변 retrieval_chain: {response_retrieval.get("answer")}')  # ⭐⭐문서에 fit한 답변 - dict(input, context, answer)
+
+
+
+def lcel_rag():
+    print('...')
     # 새로운 프롬프트 템플릿
     # template = """다음 컨텍스트를 사용해서 마지막 질문에 한글로 답하세요.
     # 답을 모르면 모른다고 하고, 답을 지어내려고 하지 마세요.
@@ -43,8 +71,19 @@ def basic_rag():
     #
     # 도움이 되는 답변:(한글로 친절하게 답변)"""
 
+def search_without_rag():
+    prompt = PromptTemplate.from_template(query)
+    llm = ChatOpenAI(model='gpt-4o-mini')
+    chain = prompt | llm
+    response = chain.invoke(input={})
+    print(f'🔥🔥최종 답변(rag없이): {response}') # ⭐일반적 답변
+
+
 
 if __name__ == '__main__':
+    # print('rag없이 기본 질문하기..')
+    # search_without_rag()
+
     print('basic_rag 검색하기..')
     basic_rag()
 
